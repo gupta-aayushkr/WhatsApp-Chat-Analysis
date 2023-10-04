b2ff642203440f5ecc309169443039678839c3b8
gupta-aayushkr <gupta.aayushkr@gmail.com>
2023-10-04 18:08:02 +0530

    ui changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import re
 from wordcloud import WordCloud
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.set_page_config(
     page_icon="💬",
@@ -57,9 +58,7 @@ if uploaded_file is not None:
     # st.write(df)
 
     #Users
-    users_list = df["user"].unique().tolist()
-    if len(users_list) > 2:
-        users_list.remove(users_list[0])
+    users_list = df["user"].value_counts()[df["user"].value_counts()>10].reset_index()['user'].tolist()
     users_list.sort()
     users_list.insert(0, "Overall")        
     selected_user = st.sidebar.selectbox("Select User to Analyze", users_list)
@@ -81,32 +80,28 @@ if uploaded_file is not None:
     #After selecting displaying some basic stats
     if st.sidebar.button("Show Analysis"):
         st.title("Statistics")
-        c1,c2,c3,c4 = st.columns(4)
+        c1,c2 = st.columns(2)
+        nested_c1_left, nested_c1_right = c1.columns(2)
 
-        #c1
-        c1.header("Total Messages")
-        c1.title(num_messages)
-
-        #c2
-        c2.header("Total Words")
-        c2.title(num_words)
-
-        #c3
-        c3.header("Media Shared")
-        c3.title(num_media)
-
-        #c3
-        c4.header("Time Spent(Days)")
-        c4.title(round((num_words/(60*24)),2))
+        #Metrics
+        nested_c1_left.metric("Total Messages", num_messages)
+        nested_c1_left.metric("Total Words", num_words)
+        nested_c1_right.metric("Media Shared", num_media)
+        nested_c1_right.metric("Time Spent(Days)",round((num_words/(60*24)),2))
+    
+        #Dataframe
+        c2.write("WhatsApp Messages")
+        c2.dataframe(df, height=300)
 
     # finding the busiest users in the group(Group level)
         if selected_user == 'Overall':
-            st.title('Most Busy Users')
+            st.title('Most Active Users in Group')
             active_users_by_msg = df["user"].value_counts()[df["user"].value_counts()>10].reset_index().head(10)
             overall_active_user_percent_df = round(df["user"].value_counts()[df["user"].value_counts()>10]/df.shape[0]*100).reset_index().rename(columns={'user':'Name', 'count':'Percent'})
             col1, col2 = st.columns(2)
 
             with col1:
+                plt.figure(figsize=(10, 4))
                 plt.bar(active_users_by_msg["user"],active_users_by_msg["count"])
                 plt.xlabel("UserName")
                 plt.ylabel("Message Counts")
@@ -114,7 +109,8 @@ if uploaded_file is not None:
                 plt.xticks(rotation='vertical')
                 st.pyplot(plt)
             with col2:
-                st.dataframe(overall_active_user_percent_df, width=1000, height=300)
+                st.header("Most Messaged (Percent)")
+                st.dataframe(overall_active_user_percent_df, width=1000)
         
     #worldcloud for each user
         st.header("WordCloud")
@@ -156,6 +152,25 @@ if uploaded_file is not None:
             plt.imshow(wordcloud, interpolation='bilinear')
             plt.axis('off')
             st.pyplot(plt)
+
+# Preferred Media Type
+        st.header("Preferred Media Type")
+        media_msg = df[df["message"].str.contains('omitted')]["message"].to_list()
+        media_type = []
+        for i in media_msg:
+            new = re.sub('^a-zA-Z','', i)
+            new = new.split()[0]
+            media_type.append(new)
+        top_media_df =  pd.DataFrame(media_type,columns=['Media']).value_counts().reset_index().head(5)
+        c1,c2 = st.columns(2)
+        with c1:
+            fig = px.pie(top_media_df, names='Media', values='count', title='Media Type Distribution')
+            st.plotly_chart(fig)
+        with c2:
+            st.subheader("Most Sent Media Types")
+            st.dataframe(top_media_df, width=500, height=300)
+
+
 
 # Timeline analysis
         st.title("Month & Day Activity")
@@ -226,4 +241,4 @@ if uploaded_file is not None:
             st.title("Conclusion:")
             user_most_active_weekday = df.groupby('Weekday').count()["message"].sort_values(ascending=False).reset_index()["Weekday"][1]
             user_most_active_hour = df.groupby('hour').count()["message"].sort_values(ascending=False).reset_index()["hour"][1]
-            st.info(f"{selected_user} is mostly active on {user_most_active_weekday}. And Mostly prefers to message at {user_most_active_hour}.")
+            st.info(f"{selected_user} is mostly active on {user_most_active_weekday}. And Mostly prefers to message at {user_most_active_hour}. There top media sent on the chat is {top_media_df['Media'][0]}.")
